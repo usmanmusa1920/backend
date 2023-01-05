7713dac6b701760b521944aa2c1d3d703cfb34b4
Usman Musa <usmanmusa1920@gmail.com>
2023-01-05 05:42:04 +0100

    edited `python_init_and_requests_pkg` directory

--- python_init_and_requests_pkg/main.py
+++ python_init_and_requests_pkg/main.py
@@ -2,8 +2,6 @@
 # the __init__.py file runs automatically as soon as a library is imported.
 # E.g as we say 'import f1' it look and runs the __init__.py file
 
-
-import req
 import f1
 
 
@@ -15,7 +13,3 @@ class A:
     
     
 # print(type(A.__new__(2, x=1)).init(4, X=1))
-
-# print(dir(req))
-# r = req.get('http://127.0.0.1:8000')
-# print(r.text)
