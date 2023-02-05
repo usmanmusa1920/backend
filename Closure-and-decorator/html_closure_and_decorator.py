84757e769819fb2693e0cf84f7517fdff2c981cf
Usman Musa <usmanmusa1920@gmail.com>
2023-02-05 07:51:53 +0100

    edited `html_closure_and_decorator` files

--- Closure-and-decorator/html_closure_and_decorator.py
+++ Closure-and-decorator/html_closure_and_decorator.py
@@ -31,60 +31,17 @@ def html_tag(tag):
     return print_tag
 
 h1_tag = html_tag('h1')
-
 # print(h1_tag) # this will print ``` <function html_tag.<locals>.print_tag at 0x7f9c808879d0> ```
 # print(h1_tag('This is a h1 tag of html (headline!)'))
 # Output:
 #   <h1>This is a h1 tag of html (headline!)</h1>
 
 
-"""   Decorator code   """
-# # Decorators
-# from functools import wraps
-
-
-# def my_logger(orig_func):
-#     import logging
-#     logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
-
-#     @wraps(orig_func)
-#     def wrapper(*args, **kwargs):
-#         logging.info(
-#             'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
-#         return orig_func(*args, **kwargs)
-
-#     return wrapper
-
-
-# def my_timer(orig_func):
-#     import time
-
-#     @wraps(orig_func)
-#     def wrapper(*args, **kwargs):
-#         t1 = time.time()
-#         result = orig_func(*args, **kwargs)
-#         t2 = time.time() - t1
-#         print('{} ran in: {} sec'.format(orig_func.__name__, t2))
-#         return result
-
-#     return wrapper
-
-# import time
-
-
-# @my_logger
-# @my_timer
-# def display_info(name, age):
-#     time.sleep(1)
-#     print('display_info ran with arguments ({}, {})'.format(name, age))
-
-# display_info('Tom', 22)
-
+"""   Decorators code   """
 from functools import wraps
 import time
-def my_timer(orig_func):
-    import time
 
+def my_timer(orig_func):
     # @wraps(orig_func)
     def wrapper(*args, **kwargs):
         t1 = time.time()
@@ -98,14 +55,14 @@ def my_timer(orig_func):
 def display_info(name, age):
     time.sleep(1)
     print('display_info ran with arguments ({}, {})'.format(name, age))
+
 display_info('Usman', 19)
 # Output:
-#   display_info ran with arguments (Usman, 19)
-#   display_info ran in: 1.0009398460388184 sec
-
+#   display_info ran with arguments (Usman, 19) # this is, the function output
+#   display_info ran in: 1.0009398460388184 sec # this is, the decorator output
 
 
 def c_alpha(v):
     return 1 if v == 5 else 2 # this is a one line if else statement
-# print(c_alpha(5))
-# print(c_alpha(3))
+# print(c_alpha(5)) # output: 1
+# print(c_alpha(3)) # output: 2
